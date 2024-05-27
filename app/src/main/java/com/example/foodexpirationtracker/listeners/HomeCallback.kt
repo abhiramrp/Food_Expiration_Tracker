2affe8ad5032bd5cc674d533b738a900893d9a0b
Abhiram Rishi Prattipati <pabhiramrishi@gmail.com>
2024-05-27 16:00:03 -0500

    resetting app

--- app/src/main/java/com/example/foodexpirationtracker/listeners/HomeCallback.kt
+++
@@ -1,5 +0,0 @@
-package com.example.foodexpirationtracker.listeners
-
-interface HomeCallback {
-    fun onRefresh()
-}
--- app/src/main/java/com/example/foodexpirationtracker/listeners/IngredientListener.kt
+++
@@ -1,7 +0,0 @@
-package com.example.foodexpirationtracker.listeners
-
-import com.example.foodexpirationtracker.ingredient.Ingredient
-
-interface IngredientListener {
-    fun onLayoutClick(item: Ingredient?)
-}
